b9846184fb455c04f13a32e49a242e654d91f620
Hobbit-li <hobbit.ring.ting@gmail.com>
2025-07-23 15:51:31 +0800

    Update data_utils.py

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,13 +1,21 @@
 def paid_split(X, y, payer_tag):
+    
+    '''
+    split the users
+    set 1: HAVE Unpaid until the current peroid
+    set 2: HAVE Paid until the peroid
+    '''
+    
     existing_payer_tag = [col for col in payer_tag if col in X.columns]
     if not existing_payer_tag:
-        raise ValueError("❌ X中不包含任何payer_tag列，无法识别未付费用户。")
-    # 特征周期内未付费样本（集合1）
+        raise ValueError("❌ None of the payer_tag columns are present in X, unable to identify unpaid users.")
+        
+    # Unpaid samples during the feature period (Set 1)
     mask_unpaid = (X[existing_payer_tag].sum(axis=1) == 0)
     X1 = X[mask_unpaid]
     y1 = y[mask_unpaid] 
 
-    # 特征周期内已付费样本（集合2）
+    # Paid samples during the feature period (Set 2)
     mask_paid = ~mask_unpaid
     X2 = X[mask_paid]
     y2 = y[mask_paid]
