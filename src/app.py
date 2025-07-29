509fcb0183d7cb7e4fba76c624ea5c5c23c8ac89
Hobbit-li <hobbit.ring.ting@gmail.com>
2025-07-29 19:35:37 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -37,6 +37,8 @@ try:
 
             temp_result = data_preprocess(df_ref, config)
             temp_result_pred = data_preprocess(df_pred, config, train_data=False)
+            st.write("✅ 脚本已加载，无语法错误")
+
 
         with st.spinner("训练模型中..."):
             model_results = {}
