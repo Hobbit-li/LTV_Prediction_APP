734216197e67fbdb6da8166c0e22860c45d62abb
Hobbit-li <hobbit.ring.ting@gmail.com>
2025-08-06 20:39:00 +0800

    Update visual.py

--- src/visual.py
+++ src/visual.py
@@ -16,17 +16,16 @@ from sklearn.metrics import (
 )
 
 
-def compare_plot(preds_results, config):
+def compare_plot(preds_results, cycles):
     """
     Objective: show the actual and the predicted LTV increasingly
     days_list_existed: have existed days for valid
     preds_results: the predicted results by running the trained model
     """
-    days_list_existed = config["days_list_existed"]
-    for day in days_list_existed:
+    for i in range(cycles):
         plt.figure(figsize=(20, 6))
         result_df_sorted = (
-            preds_results[day].sort_values(by="actual").reset_index(drop=True)
+            preds_results[i].sort_values(by="actual").reset_index(drop=True)
         )
         plt.plot(result_df_sorted["actual"], label="Actual", color="blue", linewidth=2)
         plt.plot(
@@ -37,9 +36,9 @@ def compare_plot(preds_results, config):
             linestyle="--",
         )
 
-        plt.title(f"Model Prediction vs Actual--DAY{day}")
+        plt.title(f"Model Prediction vs Actual--Month Cycle--{i+1}")
         plt.xlabel("Sample Index")
-        plt.ylabel("LTV")
+        plt.ylabel("ltv_sum")
         plt.legend()
         plt.grid(True)
         plt.tight_layout()
@@ -47,18 +46,17 @@ def compare_plot(preds_results, config):
 
 
 # Indicator evaluation
-def evaluate_ltv(preds_results, config):
+def evaluate_ltv(preds_results, cycles):
     """
     return RMSE, MAE, MSLE, R2
     user sample
     days_list_existed: have existed days for valid
     preds_results: the predicted results by running the trained model
     """
-    days_list_existed = config["days_list_existed"]
     eval_dict = {}
-    for day in days_list_existed:
-        y_true = preds_results[day]["actual"].values
-        y_pred = preds_results[day]["pred"].values
+    for i in range(cycles):
+        y_true = preds_results[i]["actual"].values
+        y_pred = preds_results[i]["pred"].values
         rmse = mean_squared_error(y_true, y_pred)
         mae = mean_absolute_error(y_true, y_pred)
         msle = mean_squared_log_error(y_true, y_pred)
@@ -74,20 +72,20 @@ def evaluate_ltv(preds_results, config):
 
 
 # Residual analysis
-def residual_plot(preds_results, config):
+def residual_plot(preds_results, cycles):
     """
     Objective: show the residuals of LTV
     days_list_existed: have existed days for valid
     preds_results: the predicted results by running the trained model
     """
     days_list_existed = config["days_list_existed"]
-    for day in days_list_existed:
+    for i in range(cycles):
         residuals = (
-            preds_results[day]["pred"].values - preds_results[day]["actual"].values
+            preds_results[i]["pred"].values - preds_results[i]["actual"].values
         )
         plt.figure(figsize=(20, 4))
         plt.plot(residuals, marker="o", linestyle="-", color="purple", alpha=0.7)
-        plt.title(f"Residuals Over Samples--DAY{day}")
+        plt.title(f"Residuals Over Samples--Month Cycle--{i+1}")
         plt.xlabel("Sample Index")
         plt.ylim(-5000, 5000)
         plt.ylabel("Residual (Prediction Error)")
