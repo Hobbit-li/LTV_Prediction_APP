cfc8d10185b4a1bd62b7a79ce119a82c2b39035d
Hobbit-li <hobbit.ring.ting@gmail.com>
2025-08-15 00:10:31 +0800

    Update results_show.py

--- src/results_show.py
+++ src/results_show.py
@@ -27,7 +27,7 @@ def show_roas_ltv(preds_results, cost, existed_tag, cycles=10):
       }
     """
     result = {}
-    ltv_existed = preds_results[existed_tag].sum(axis=1)
+    ltv_existed = preds_results[0][existed_tag].sum(axis=1)
     y_pred = preds_results[0]["pred"]
     y_actual = preds_results[0]["actual"]
     for i in range(cycles):
